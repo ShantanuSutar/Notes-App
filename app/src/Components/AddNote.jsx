f4eff1860894dd4ddd9e41b65ab67a4a75323931
Shantanu Sutar <shantanusutar17@gmail.com>
2023-03-25 00:11:58 +0530

    taking the input from user
    using useState we take the input event and set its value to noteText

--- app/src/Components/AddNote.jsx
+++ app/src/Components/AddNote.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 const AddNote = () => {
+  const [noteText, setNoteText] = useState("");
+
+  const handleChange = (e) => {
+    setNoteText(e.target.value);
+  };
+
   return (
     <div className="note new">
-      <textarea cols="10" rows="8" placeholder="Type to add a note"></textarea>
+      <textarea
+        cols="10"
+        rows="8"
+        placeholder="Type to a dd a note"
+        value={noteText}
+        onChange={handleChange}
+      ></textarea>
       <div className="note-footer">
         <small>200 Remaining</small>
         <button className="save">Save</button>
