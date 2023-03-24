607c4b018ea56f94027647483453c5db500fdca0
Shantanu Sutar <shantanusutar17@gmail.com>
2023-03-24 21:03:10 +0530

    creating notes dynamically
    using state we create new notes with text, id and date
    
    we pass all notes with text, id and date as parameters to the notesList component
    
    In notesList component, we de-structure it and map over every note and pass the same parameters into Note component
    
    And Note component just displays the text and date passed into id

---
+++ app/src/App.jsx
@@ -0,0 +1,39 @@
+import NotesList from "./Components/NotesList";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "15/07/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "24/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "5/09/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my fourth note!",
+      date: "10/11/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my fifth note!",
+      date: "1/8/2022",
+    },
+  ]);
+  return (
+    <div className="container">
+      <NotesList notes={notes} />
+    </div>
+  );
+}
+
+export default App;
